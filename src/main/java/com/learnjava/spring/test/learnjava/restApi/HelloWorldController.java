588e033202eadd0e4a20a90b065b51149c8b7503
Sobhit K <sobhit2010@gmail.com>
2023-11-26 21:02:45 +0530

    Adding Security to actuators and Accessing custom application.properties

--- src/main/java/com/learnjava/spring/test/learnjava/restApi/HelloWorldController.java
+++ src/main/java/com/learnjava/spring/test/learnjava/restApi/HelloWorldController.java
@@ -1,10 +1,23 @@
 package com.learnjava.spring.test.learnjava.restApi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach Name: " + coachName + "\n Team Name : " + teamName;
+    }
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";
