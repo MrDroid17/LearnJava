33ab0bed1bb2664557eb374fb2ef0435b66c99fb
Sobhit K <sobhit2010@gmail.com>
2024-01-05 15:44:56 +0530

    Using commandLineRunner and database connection

--- src/main/java/com/learnjava/spring/test/learnjava/LearnjavaApplication.java
+++ src/main/java/com/learnjava/spring/test/learnjava/LearnjavaApplication.java
@@ -1,7 +1,9 @@
 package com.learnjava.spring.test.learnjava;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //@SpringBootApplication(
 //		scanBasePackages = {"com.learnjava.spring.test.learnjava", "com.learnjava.spring.test.utils"}
@@ -12,5 +14,12 @@ public class LearnjavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnjavaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 }
