2e3e2627180c1df891461baaf54777f7d3ca40aa
Sobhit K <sobhit2010@gmail.com>
2024-07-12 13:38:54 +0530

    add new path variable api

--- src/main/java/com/learnjava/spring/test/Project_03_RestCRUDApi/rest/StudentRestController.java
+++ src/main/java/com/learnjava/spring/test/Project_03_RestCRUDApi/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.learnjava.spring.test.Project_03_RestCRUDApi.rest;
 
 import com.learnjava.spring.test.Project_03_RestCRUDApi.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+     private List<Student>  students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-
-        List<Student> students = new ArrayList<>();
+     // define @PostConstruct to load the student data .... only once
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
         students.add(new Student("Sobhit", "Kumar"));
         students.add(new Student("Shweta", "Singh"));
         students.add(new Student("Rohan", "Singh"));
+        System.out.println(students);
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentsById(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
