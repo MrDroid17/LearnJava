a70b5213e470948d8d38834696b9943411059184
Sobhit K <sobhit2010@gmail.com>
2024-07-12 13:38:54 +0530

    add new path variable api

---
+++ src/main/java/com/learnjava/spring/test/Project_03_RestCRUDApi/entity/StudentNotFoundException.java
@@ -0,0 +1,15 @@
+package com.learnjava.spring.test.Project_03_RestCRUDApi.entity;
+
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
