024100b54bd77d21a3b3fa4558eaf2457713cb83
Sobhit K <sobhit2010@gmail.com>
2024-07-14 17:45:11 +0530

    Add spring data rest api  size, page, sorting

--- src/main/java/com/learnjava/spring/test/Project_06_CRUD_Employee_Spring_Data_JPA_REST/dao/EmployeeRepository.java
+++ src/main/java/com/learnjava/spring/test/Project_06_CRUD_Employee_Spring_Data_JPA_REST/dao/EmployeeRepository.java
@@ -2,8 +2,14 @@ package com.learnjava.spring.test.Project_06_CRUD_Employee_Spring_Data_JPA_REST.
 
 import com.learnjava.spring.test.Project_06_CRUD_Employee_Spring_Data_JPA_REST.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+/*
+* to change api path from employees to members
+* */
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it
+
 }
