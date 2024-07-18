654805f19d098d5c49781b8fb54edb225b8980f5
Sobhit K <sobhit2010@gmail.com>
2024-07-18 12:25:53 +0530

    Using data binding with text, dropdown, radiobutton, checkbox

--- src/main/java/com/learnjava/spring/test/Project_08_Spring_MVC_Thymeleaf/controller/StudentController.java
+++ src/main/java/com/learnjava/spring/test/Project_08_Spring_MVC_Thymeleaf/controller/StudentController.java
@@ -1,20 +1,43 @@
 package com.learnjava.spring.test.Project_08_Spring_MVC_Thymeleaf.controller;
 
 import com.learnjava.spring.test.Project_08_Spring_MVC_Thymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
         Student student = new Student();
         theModel.addAttribute("student", student);
-
+        theModel.addAttribute("countries", countries);
+        theModel.addAttribute("languages", languages);
+        theModel.addAttribute("systems", systems);
 
         return "student-form";
+
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student){
+        System.out.printf("the student "+ student.getFirstName() + " " + student.getLastName());
+        return "student-confirmation";
 
     }
 }
--- src/main/java/com/learnjava/spring/test/Project_08_Spring_MVC_Thymeleaf/model/Student.java
+++ src/main/java/com/learnjava/spring/test/Project_08_Spring_MVC_Thymeleaf/model/Student.java
@@ -1,9 +1,17 @@
 package com.learnjava.spring.test.Project_08_Spring_MVC_Thymeleaf.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
+
+    private String country;
+
+    private String favouriteLanguage;
+
+    private List<String> favouriteSystems;
 
     public Student(){
 
@@ -23,5 +31,29 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public List<String> getFavouriteSystems() {
+        return favouriteSystems;
+    }
+
+    public void setFavouriteSystems(List<String> favouriteSystems) {
+        this.favouriteSystems = favouriteSystems;
     }
 }
