335816a275373f8ae974b3886c6e22ddbe51b741
Sobhit K <sobhit2010@gmail.com>
2024-07-21 11:52:23 +0530

    Spring MVC with thymeleaf CRUD

--- src/main/java/com/learnjava/spring/test/Project_10_Spring_MVC_CRUD/SpringMVCDatabaseCRUDwithThymeleaf.java
+++ src/main/java/com/learnjava/spring/test/Project_10_Spring_MVC_CRUD/SpringMVCDatabaseCRUDwithThymeleaf.java
@@ -1,15 +1,14 @@
 package com.learnjava.spring.test.Project_10_Spring_MVC_CRUD;
 
-import com.learnjava.spring.test.Project_05_CRUD_Employee_Spring_data_JPA.CrudEmployeeSpringDataJPAApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringMVCDatabaseCRUD {
+public class SpringMVCDatabaseCRUDwithThymeleaf {
     public static void main(String[] args) {
-        SpringApplication.run(SpringMVCDatabaseCRUD.class, args);
+        SpringApplication.run(SpringMVCDatabaseCRUDwithThymeleaf.class, args);
     }
 
     @Bean
--- src/main/java/com/learnjava/spring/test/Project_10_Spring_MVC_CRUD/dao/EmployeeRepository.java
+++ src/main/java/com/learnjava/spring/test/Project_10_Spring_MVC_CRUD/dao/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.learnjava.spring.test.Project_10_Spring_MVC_CRUD.dao;
 import com.learnjava.spring.test.Project_10_Spring_MVC_CRUD.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it
+    // add a method to sort by lastaName
+    public List<Employee> findAllByOrderByLastNameAsc();
+
 }
