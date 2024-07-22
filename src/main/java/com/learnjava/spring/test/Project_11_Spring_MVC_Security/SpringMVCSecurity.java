f18e2c9edfbf8671710929cf2866a9bac8f867cc
Sobhit K <sobhit2010@gmail.com>
2024-07-22 06:04:54 +0530

    Project 11 Starter code setup mvc-security

---
+++ src/main/java/com/learnjava/spring/test/Project_11_Spring_MVC_Security/SpringMVCSecurity.java
@@ -0,0 +1,21 @@
+package com.learnjava.spring.test.Project_11_Spring_MVC_Security;
+
+import com.learnjava.spring.test.Project_10_Spring_MVC_CRUD.SpringMVCDatabaseCRUDwithThymeleaf;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringMVCSecurity {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMVCSecurity.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args){
+        return runner -> {
+            System.out.println("Hello World");
+        };
+    }
+}
---
+++ src/main/java/com/learnjava/spring/test/Project_11_Spring_MVC_Security/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.learnjava.spring.test.Project_11_Spring_MVC_Security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+}
